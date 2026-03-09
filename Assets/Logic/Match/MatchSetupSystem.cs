1048749bcb3e70c6f9ceb4d5a90f89b4436b668e
LucileVE <lucile.verneret@gmail.com>
2026-03-09 22:12:02 +0100

    Ton message ici

--- Assets/Logic/Match/MatchSetupSystem.cs
+++ Assets/Logic/Match/MatchSetupSystem.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Collections;
+    
 public class MatchSetupSystem : MonoBehaviour
 {
     [SerializeField] private HeroData heroData;
@@ -15,11 +16,19 @@ public class MatchSetupSystem : MonoBehaviour
 
     public void OnStartGame()
     {
+        StartCoroutine(InitializeAfterDelay());
+    }
+
+    private IEnumerator InitializeAfterDelay()  // ← Retour "IEnumerator" obligatoire
+    {
+        yield return new WaitForSeconds(15f);  // ← Fonctionne maintenant
+        
+        // Votre code s'exécute après 15s
         HeroSystem.Instance.Setup(heroData);
         EnemySystem.Instance.Setup(enemyDatas);
         CardSystem.Instance.Setup(heroData.Deck);
         PerkSystem.Instance.AddPerk(new Perk(perkData));
-        GADrawCards gaDrawCards = new (5);
-        ActionSystem.Instance.Perform(gaDrawCards);    
+        GADrawCards gaDrawCards = new(5);
+        ActionSystem.Instance.Perform(gaDrawCards);
     }
 }
